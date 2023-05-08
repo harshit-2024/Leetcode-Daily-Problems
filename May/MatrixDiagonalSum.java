dc2698e160accf834f8d99edde8a1849e7acd06f
Harshit <harshit.ranjansingh2020@gmail.com>
2023-05-08 16:54:03 +0530

    08-05-2023

---
+++ May/MatrixDiagonalSum.java
@@ -0,0 +1,18 @@
+public class MatrixDiagonalSum {
+    public int diagonalSum(int[][] mat) {
+        int sum = 0;
+        int n = mat.length;
+        for(int i=0;i<n;i++){
+            sum += mat[i][i];
+            sum += mat[i][n-i-1];
+        }
+        if(n%2==1)
+            sum -= mat[n/2][n/2];
+        return sum;
+    }
+}
+
+/*
+ * Time Complexity: O(N)
+ * Space Complexity: O(1)
+ */
